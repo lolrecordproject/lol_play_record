61698816a8f793aced3e31859e6d2fea931cdbd0
intae0099 <dlsxo14700@gmail.com>
2024-03-25 15:52:01 +0900

    first commit

---
+++ src/main/java/project_team/lol_play_record/LolPlayRecordApplication.java
@@ -0,0 +1,13 @@
+package project_team.lol_play_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolPlayRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolPlayRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project_team/lol_play_record/LolPlayRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package project_team.lol_play_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolPlayRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
